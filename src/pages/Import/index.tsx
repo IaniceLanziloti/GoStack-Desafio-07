300cf8f45ae854a2644f04b74f4ad266db42797f
Ianice Lanziloti <ianice_lanziloti@hotmail.com>
2020-07-16 21:32:11 -0300

    Fazendo upload do primeiro arquivo da lista e enviando para o backend

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,25 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
+    const data = new FormData();
+    data.append('file', uploadedFiles[0].file);
     try {
-      // await api.post('/transactions/import', data);
+      console.log('upload');
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const file = files[files.length - 1];
+
+    const uploadedFile: FileProps = {
+      file,
+      name: file.name,
+      readableSize: file.size.toString(),
+    };
+    setUploadedFiles([...uploadedFiles, uploadedFile]);
   }
 
   return (
